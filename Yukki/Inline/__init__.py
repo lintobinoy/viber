73b590bed44e4ca159e3afbb25b2173142511a17
lintobinoy <71056969+lintobinoy@users.noreply.github.com>
2022-03-16 15:09:19 +0530

    Update __init__.py

--- Yukki/Inline/__init__.py
+++ Yukki/Inline/__init__.py
@@ -1,3 +1,4 @@
+from .lyrics import lyricssex
 from .others import download_markup, others_markup
 from .play import (audio_markup, audio_markup2, audio_timer_markup_start,
                    primary_markup, search_markup, search_markup2,
@@ -12,4 +13,3 @@ from .start import (custommarkup, dashmarkup, private_panel, setting_markup,
                     setting_markup2, start_pannel, usermarkup, volmarkup)
 from .stats import stats1, stats2, stats3, stats4, stats5, stats6, stats7
 from .videocalls import choose_markup, livestream_markup, stream_quality_markup
-from .lyrics import (lyricssex)
